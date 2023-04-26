def16e2b3d5e974fc067c405befcdc7b1284f552
bogdancomardici <115560133+bogdancomardici@users.noreply.github.com>
2023-04-27 01:08:37 +0300

    locatie-automata

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const app = express();
-
-
+var ipapi = require('ipapi.co');
 
 
 require("dotenv").config();
@@ -22,6 +21,8 @@ app.set("view engine", "ejs");
 app.get("/", function (req, res) {
     // It will not fetch and display any data in the index page
     res.render("index", { weather: null, error: null });
+
+
 });
 
 app.post('/', function (req, res) {
@@ -90,6 +91,90 @@ app.post('/', function (req, res) {
     })
 });
 
+var city_auto = '';
+
+var callback = function(res){
+    console.log(res.city);
+    city_auto = res.city;
+};
+
+app.get("/locatie_automata", function (req, res) {
+
+    // let ip = req.ip; daca aplicatia ar fi pe net
+    // let ip = '188.24.29.24'; Cluj
+    // Bucuresti
+    let ip = '45.250.65.105';
+
+    // Get city name passed in the form
+    ipapi.location(callback, ip);
+    console.log(city_auto);
+    let city = city_auto;
+    // Use that city name to fetch data
+    // Use the API_KEY in the '.env' file
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+    request(url, function (err, response, body) {
+
+        // On return, check the json data fetched
+        if (err) {
+            res.render('index', { weather: null, error: 'Error, please try again' });
+        } else {
+            let weather = JSON.parse(body);
+
+            console.log(weather);
+
+            if (weather.main == undefined) {
+                res.render('index', { weather: null, error: 'Error, please try again' });
+            } else {
+                // we shall use the data got to set up your output
+                let place = `${weather.name}, ${weather.sys.country}`,
+                    /* you shall calculate the current timezone using the data fetched*/
+                    weatherTimezone = `${new Date(
+                        weather.dt * 1000 - weather.timezone * 1000
+                    )}`;
+                let weatherTemp = `${weather.main.temp}`,
+                    weatherPressure = `${weather.main.pressure}`,
+                    /* you will fetch the weather icon and its size using the icon data*/
+                    weatherIcon = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`,
+                    weatherDescription = `${weather.weather[0].description}`,
+                    humidity = `${weather.main.humidity}`,
+                    clouds = `${weather.clouds.all}`,
+                    visibility = `${weather.visibility}`,
+                    main = `${weather.weather[0].main}`,
+                    weatherFahrenheit;
+                weatherFahrenheit = (weatherTemp * 9) / 5 + 32;
+
+                // you shall also round off the value of the degrees fahrenheit calculated into two decimal places
+                function roundToTwo(num) {
+                    return +(Math.round(num + "e+2") + "e-2");
+                }
+                weatherFahrenheit = roundToTwo(weatherFahrenheit);
+
+                res.render("index", {
+                    weather: weather,
+                    place: place,
+                    temp: weatherTemp,
+                    pressure: weatherPressure,
+                    icon: weatherIcon,
+                    description: weatherDescription,
+                    timezone: weatherTimezone,
+                    humidity: humidity,
+                    fahrenheit: weatherFahrenheit,
+                    clouds: clouds,
+                    visibility: visibility,
+                    main: main,
+                    error: null,
+                });
+            }
+        }
+    })
+});
+
+
 app.listen(5000, function () {
     console.log("Weather app listening on port 5000!");
-  });
+});
+
+
+
+
