463676ededc69ada78306c5c44154941a89e1a31
TimiAndrei <timiandrei223@gmail.com>
2023-04-27 23:15:07 +0300

    added functionality and checks for register

--- server.js
+++ server.js
@@ -2,20 +2,25 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const app = express();
-
-
-
+const bcrypt = require("bcrypt");
+const { pool } = require("./dbConfig");
+const session = require("express-session");
+const flash = require("express-flash");
 require("dotenv").config();
-
-
 
 const apiKey = `${process.env.API_KEY}`;
 
-// Setup your express app and body-parser configurations
-// Setup your javascript template view engine
-// we will serve your static pages from the public directory, it will act as your root directory
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+    secret: "secret",
+
+    resave: false,
+
+    saveUninitialized: false
+}));
+app.use(flash());
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
@@ -33,6 +38,54 @@ app.get("/users/login", (req, res) => {
 
 app.get("/users/dashboard", (req, res) => {
     res.render("dashboard", { user: "John Doe" });
+});
+
+app.post("/users/register", async (req, res) => {
+    let { name, email, password, password2 } = req.body;
+
+    let errors = [];
+
+    if (!name || !email || !password || !password2) {
+        errors.push({ message: "Please enter all fields!" });
+    }
+
+    if (password.length < 6) {
+        errors.push({ message: "Password should be at least 6 characters!" });
+    }
+
+    if (password != password2) {
+        errors.push({ message: "Passwords do not match!" });
+    }
+
+    if (errors.length > 0) {
+        console.log(errors);
+        res.render("register", { errors });
+    } else {
+        let encryptedPass = await bcrypt.hash(password, 10);
+
+        pool.query(
+            `SELECT * FROM USERS WHERE email=$1`, [email], (err, results) => {
+                if (err) {
+                    throw err;
+                }
+                if (results.rows.length > 0) {
+                    errors.push({ message: "Email already registered!" });
+                    res.render("register", { errors });
+                }
+                else {
+                    pool.query(
+                        `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id, password`, [name, email, encryptedPass], (err, results) => {
+                            if (err) {
+                                throw err;
+                            }
+                            req.flash('success_msg', "You have successfully registered. Please login to continue.");
+                            res.redirect("/users/login");
+                        }
+                    )
+                }
+            }
+        )
+    }
 });
 
 app.post('/', function (req, res) {
